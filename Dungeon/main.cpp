97b487d2b5c1b9fab99cd85821bb3fdbd0e3adde
ccl0310 <frgnd5433@gmail.com>
2023-05-23 12:55:52 +0800

    Upd 5/23

--- Dungeon/main.cpp
+++ Dungeon/main.cpp
@@ -1,4 +1,8 @@
 #include <iostream>
+#include <vector>
+#include "Item.h"
+#include "Sword.h"
+#include "HealthPotion.h"
 using namespace std;
 
 const char WALL = '#';
@@ -6,7 +10,11 @@ const char BLANK = ' ';
 
 char map[3][8][8] = { {{BLANK}} };
 
+vector<Item*> items;
+
 int main()
 {
+	items.push_back(new Sword(8, 8));
+	items.push_back(new HealthPotion(8, 8));
 	return 0;
 }
